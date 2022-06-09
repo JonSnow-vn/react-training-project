b271f86fb0539861c8feaa0e907db68b701c3edc
JonSnow-vn <toiselamduoc0110@gmail.com>
2022-06-09 08:59:16 +0700

    commit 1

--- src/components/user-form/UserFormComponent.tsx
+++ src/components/user-form/UserFormComponent.tsx
@@ -126,6 +126,9 @@ function UserFormComponent() {
                         {userId && <Button type="button" label='Delete' action={() => { handleDelete(Number(userId)) }} typeButton="button" margin='0 8px 0 0' />}
                         <Button type="link" label='Cancel' action={"/"} />
                     </div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </Form>
             )}
         </Formik>
